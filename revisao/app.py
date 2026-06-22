186bf8601e7d35c9d664f99b956b79d33de4ed1d
samara-tailine <samaratailineo@gmail.com>
2026-06-22 07:48:07 -0300

    Atividade sobre Pandas 2

---
+++ revisao/app.py
@@ -0,0 +1,27 @@
+#1) Importar a biblioteca pandas
+import pandas as pd
+
+#2) Carregar uma planilha do Excel:
+#Lê o arquivo que está no caminho e armazena na variável  df
+df = pd.read_excel('revisao\\planilha.xlsx')
+print(df.head())
+
+#3) Inserir novos dados na planilha
+
+
+#4) Atualizar dados na planilha
+
+
+#5) Filtrar dados
+
+
+#6) Classificar dados
+
+
+#7) Contar dados
+
+
+#8) Agrupar dados
+
+
+#9) Exportar dados
